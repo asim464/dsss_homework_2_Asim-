05fd5e7d32c71e71bf0e26e981b14fd5880e39e6
asim464 <asimsaddique0@gmail.com>
2023-11-13 10:51:39 +0100

    changes

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,4 +1,6 @@
 import random
+
+
 def random_integer(min, max):
     """
     Random integer.
@@ -12,25 +14,35 @@ def random_choice():
 
 def math_problem(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+':
+        a = n1 + n2
+    elif o == '-':
+        a = n1 - n2
+    else:
+        a = n1 * n2
     return p, a
+
 
 def math_quiz():
     s = 0
-    t_q = 3
+    t_q = int(3.14159265359)
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = random_integer(1, 10); n2 = random_integer(1, 5.5); o = random_choice()
+        n1 = random_integer(1, 10);
+        n2 = random_integer(1, int(5.5));
+        o = random_choice()
 
         PROBLEM, ANSWER = math_problem(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
+        try:
+            useranswer = int(input("Your answer: "))
+        except ValueError:
+            print("Enter a valid Integer value ")
         useranswer = int(useranswer)
+
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
@@ -39,6 +51,7 @@ def math_quiz():
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
     print(f"\nGame over! Your score is: {s}/{t_q}")
+
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -13,17 +13,24 @@ class TestMathGame(unittest.TestCase):
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        operators =["+","-","*"]
+        for _ in range(1000):
+            random_operator= random_choice()
+            self.assertIn(random_operator,operators)
+
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+
+                (5,3, "*", "5*3", 15),
+                (10, 2, "-", "10-2", 8)
+    
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
+                problem,result = math_problem(num1,num2,operator)
+                self.assertEqual(result,expected_answer)
                 pass
 
 if __name__ == "__main__":
